ba81bd5fa72d1754c26ae2126e5541e17d626fdf
lilJ <julia.s@live.nl>
2025-09-17 09:33:14 +0200

    layout done

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,25 +12,6 @@ const marhey = Marhey({
 })
 
 
-
-import { Geist, Geist_Mono } from "next/font/google";
-
-
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
-
-
 export const metadata: Metadata = {
   title: "Lichtsculpturen",
   description: "Kunst door Jacqueline Tiemens",
