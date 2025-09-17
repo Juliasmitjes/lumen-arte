5ef25a7d4b499995fdcea204e2f62f5c2b287b06
lilJ <julia.s@live.nl>
2025-09-17 21:16:49 +0200

    install supabase

---
+++ app/instruments/page.tsx
@@ -0,0 +1,8 @@
+import { createClient } from '../lib/supabaseServer';
+
+export default async function Instruments() {
+  const supabase = await createClient();
+  const { data: instruments } = await supabase.from("instruments").select();
+
+  return <pre>{JSON.stringify(instruments, null, 2)}</pre>
+}
