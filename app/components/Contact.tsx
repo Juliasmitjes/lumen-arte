e9e1c6280e86830a9bff2559e7d296479f490a6d
lilJ <julia.s@live.nl>
2025-11-24 20:31:18 +0100

    min-h-screen

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -54,7 +54,7 @@ const Contact = () => {
   ];
 
   return (
-    <section id="contact" className="py-20 gradient-earth">
+    <section id="contact" className=" min-h-screen flex items-center py-20 gradient-earth">
       <div className="container mx-auto px-6">
         <div className="max-w-6xl mx-auto">
           {/* header */}
